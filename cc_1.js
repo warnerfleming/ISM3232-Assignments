29f3f93f0bd31b1c080bd7ede40267aa77b73bea
Warner Fleming <fleming14@usf.edu>
2024-09-01 15:53:10 -0400

    "Task 3 - Financial Calculations".

--- cc_1.js
+++ cc_1.js
@@ -1,19 +1,33 @@
 // Scenario 1
 
-let employeeName = "Warner"
-console.log(typeof employeeName) 
-const employeeID = 1
-console.log(typeof employeeID)
-var isActive = true 
+let employeeName = "Warner";
+console.log(typeof employeeName); 
+const employeeID = 1;
+console.log(typeof employeeID);
+var isActive = true;
 console.log(typeof isActive); 
 
 // Scenario 2
 
-let products = ["cheese", "dough", "sauce"] 
+let products = ["cheese", "dough", "sauce"];
 const productDetails = {
   name 
   price
   instock
-}
-console.log(products)
+};
+console.log(products);
 console.log(productDetails);
+
+// Scenario 3
+
+let accountBalance = 1000;
+accountBalance += 1500;
+console.log(accountBalance);
+accountBalance -= 500;
+console.log(accountBalance);
+accountBalance *= 2;
+console.log(accountBalance);
+accountBalance /= 1.5; 
+console.log(accountBalance);
+accountBalance %= 5;
+console.log(accountBalance);

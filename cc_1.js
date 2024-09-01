9960f95b7e982942d5caa89246faa6beeb1d7b04
Warner Fleming <fleming14@usf.edu>
2024-09-01 13:46:47 -0400

    "Task 1 - Employee Information".

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,7 @@
 let employeeName = "Warner"
+console.log(typeof employeeName) 
 const employeeID = 1
+console.log(typeof employeeID)
 var isActive = true 
-console.log(emplyeeName, employeeID, isActive);
+console.log(typeof isActive);
+
